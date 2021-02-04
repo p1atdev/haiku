71f78cca8ef1c89747f3305439d1e5ca63199e68
Shu Teiei <shu_teiei@outlook.jp>
2021-02-04 22:08:20 +0900

    feat: 完全にランダムにした

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     with open(file_name, "r") as file:
         text = file.read().replace("\n", ".\n")
 
+    # 繰り返す回数
+    repeat = 1
+    if len(argv) >= 3:
+        repeat = int(argv[2])
+
     # メカブのやつ
     tagger = MeCab.Tagger("-Owakati")
     text = tagger.parse(text)
@@ -46,23 +51,26 @@ def main():
             length = 0
             longWord = ""
 
-    # 出力する5 7 5
-    haiku = [random.choice(fiveWord), random.choice(
-        sevenWord), random.choice(fiveWord)]
+    for _ in range(repeat):
+
+        # 出力する5 7 5
+        haiku = [random.choice(fiveWord), random.choice(
+            sevenWord), random.choice(fiveWord)]
 
     # 助詞から始まるやつを除外
-    joshi = ["の", "て", "が", "は", "を", "ん", "に", "っ"]
+        joshi = ["の", "て", "が", "は", "を", "ん",
+                 "に", "っ", "な", "り", "と", "や", "だ", "ぬ", "で"]
 
-    while (haiku[0][0] in joshi):
-        haiku[0][0] = random.choice(fiveWord)
+        while (haiku[0][0] in joshi):
+            haiku[0] = random.choice(fiveWord)
 
-    while (haiku[1][0] in joshi):
-        haiku[1][0] = random.choice(fiveWord)
+        while (haiku[1][0] in joshi):
+            haiku[1] = random.choice(sevenWord)
 
-    while (haiku[2][0] in joshi):
-        haiku[2][0] = random.choice(fiveWord)
+        while (haiku[2][0] in joshi):
+            haiku[2] = random.choice(fiveWord)
 
-    print(haiku[0], " ", haiku[1], " ", haiku[2])
+        print(haiku[0], " ", haiku[1], " ", haiku[2])
 
     # print(fiveWord)
 
